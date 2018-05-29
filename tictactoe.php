209bdf20347faf7d4912d17cf4292c0fea48f8e7
Gijs Danoe <gijs-danoe5@hotmail.com>
2018-05-29 13:58:15 +0000

    tictactoe.php created online with Bitbucket

---
+++ tictactoe.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Tic Tac Toe</title>
+    <meta name="description" content="Tic Tac Toe" />
+    <meta name="author" content="Gijs Danoe, Luuk Janssen, Thomas Tan, Benjamin Kleppe" />
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-115655085-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-115655085-1');
+    </script>
+    <link rel="stylesheet" type="text/css" href="tictactoe.css" />
+    <link href="https://fonts.googleapis.com/css?family=Mina" rel="stylesheet">
+</head>
+<body>
+<h1></h1>
+<div>
+<table class="board">
+    <tr>
+        <td class="content"></td>
+        <td class="content"></td>
+        <td class="content"></td>
+    </tr>
+    <tr>
+        <td class="content"></td>
+        <td class="content"></td>
+        <td class="content"></td>
+    </tr>
+    <tr>
+        <td class="content"></td>
+        <td class="content"></td>
+        <td class="content"></td>
+    </tr>
+</table>
+</div>
+</body>
