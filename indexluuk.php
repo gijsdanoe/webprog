5a91ccceb8ea4ec6eef1f36424ae866659c1b3d8
Luuk <l.j.janssen@student.rug.nl>
2018-05-30 17:00:24 +0000

    indexluuk.php created online with Bitbucket

---
+++ indexluuk.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Tic Tac Toe</title>
+    <meta name="description" content="Tic Tac Toe" />
+    <meta name="author" content="Gijs Danoe, Luuk Janssen, Thomas Tan, Benjamin Kleppe" />
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-115655085-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-115655085-1');
+    </script>
+    <link rel="stylesheet" type="text/css" href="tictactoe.css" />
+    <link href="https://fonts.googleapis.com/css?family=Mina" rel="stylesheet">
+</head>
+<body>
+    <div class="board">
+        <h2 id="name_display">Tic-Tac-Toe</h2>
+        <div id="row1">
+            <div onclick="play(this,1);" class="content"></div>
+            <div onclick="play(this,2);" class="content"></div>
+            <div onclick="play(this,4);" class="content"></div>
+        </div>
+        <div id="row2">
+            <div onclick="play(this,8);" class="content"></div>
+            <div onclick="play(this,16);" class="content"></div>
+            <div onclick="play(this,32);" class="content"></div>
+        </div>
+        <div id="row3">
+            <div onclick="play(this,64);" class="content"></div>
+            <div onclick="play(this,128);" class="content"></div>
+            <div onclick="play(this,256);" class="content"></div>
+        </div>
+    </div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="tictactoe.js"></script>
+</body>
