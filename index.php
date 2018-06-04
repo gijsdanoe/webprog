9dbdd23cf6aef88ea43e098ef710d95cb0adb063
Gijs Danoe <gijs-danoe5@hotmail.com>
2018-06-04 12:26:12 +0000

    index.php edited online with Bitbucket

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
+<?PHP
+if (isset($_GET['ajax'])) {
+    $data = file_get_contents("tictactoe.json");
+    header('Content-Type: application/json');
+    echo $data;
+    die();
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -15,30 +23,31 @@
         gtag('config', 'UA-115655085-1');
     </script>
     <link rel="stylesheet" type="text/css" href="tictactoe.css" />
-    <link rel="stylesheet" type="text/css" href="bootstrap-grid.min.css"/>
     <link href="https://fonts.googleapis.com/css?family=Mina" rel="stylesheet">
 </head>
 <body>
-<div>
-<table class="board">
-    <h2 id="name_display">Tic-Tac-Toe</h2>
-    <tr>
-        <td onclick="play(this);" class="content"></td>
-        <td onclick="play(this);" class="content"></td>
-        <td onclick="play(this);" class="content"></td>
-    </tr>
-    <tr>
-        <td onclick="play(this);" class="content"></td>
-        <td onclick="play(this);" class="content"></td>
-        <td onclick="play(this);" class="content"></td>
-    </tr>
-    <tr>
-        <td onclick="play(this);" class="content"></td>
-        <td onclick="play(this);" class="content"></td>
-        <td onclick="play(this);" class="content"></td>
-    </tr>
-</table>
+<header>
+    <h1>Boter-kaas-en-eieren</h1>
+    <h2 id="name_display">&nbsp;</h2>
+</header>
+<div class="board">
+    <div id="row1">
+        <div onclick="play(this,1);" class="content" id="1"></div>
+        <div onclick="play(this,2);" class="content" id="2"></div>
+        <div onclick="play(this,4);" class="content" id="3"></div>
+    </div>
+    <div id="row2">
+        <div onclick="play(this,8);" class="content" id="4"></div>
+        <div onclick="play(this,16);" class="content" id="5"></div>
+        <div onclick="play(this,32);" class="content" id="6"></div>
+    </div>
+    <div id="row3">
+        <div onclick="play(this,64);" class="content" id="7"></div>
+        <div onclick="play(this,128);" class="content" id="8"></div>
+        <div onclick="play(this,256);" class="content" id="9"></div>
+    </div>
 </div>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script src="tictactoe.js"></script>
+
 </body>
